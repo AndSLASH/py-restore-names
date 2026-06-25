10e9a957ec9b406d7457fde484e36d0b4100c451
AndSLASH <andrey.zaharov.93@gmail.com>
2026-06-25 13:13:27 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,33 @@
 import pytest
+from typing import List
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture
+def users_semple() -> List[dict]:
+    return [
+        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
+        {"last_name": "Adams", "full_name": "Mike Adams"},
+        {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
+    ]
+
+
+def test_restore_names_with_sample_users(users_semple: List[dict]) -> None:
+    restore_names(users_semple)
+
+    assert users_semple[0]["first_name"] == "Jack"
+    assert users_semple[1]["first_name"] == "Mike"
+    assert users_semple[2]["first_name"] == "Anna"
+
+
+
+def test_empty_input() -> None:
+    users = []
+    restore_names(users)
+    assert users == []
+
+
+def test_missing_first_name() -> None:
+    users = [{"last_name": "Doe"}]
+    with pytest.raises(KeyError):
+        restore_names(users)
