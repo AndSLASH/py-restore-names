805890645cc32573ce9ff691af686c55304ff94e
AndSLASH <andrey.zaharov.93@gmail.com>
2026-06-25 13:24:49 +0300

    Fix

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -6,9 +6,20 @@ from app.restore_names import restore_names
 @pytest.fixture
 def users_semple() -> List[dict]:
     return [
-        {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-        {"last_name": "Adams", "full_name": "Mike Adams"},
-        {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy"
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams"
+        },
+        {
+            "first_name": "Anna",
+            "last_name": "Smith",
+            "full_name": "Anna Smith"
+        },
     ]
 
 
@@ -18,7 +29,6 @@ def test_restore_names_with_sample_users(users_semple: List[dict]) -> None:
     assert users_semple[0]["first_name"] == "Jack"
     assert users_semple[1]["first_name"] == "Mike"
     assert users_semple[2]["first_name"] == "Anna"
-
 
 
 def test_empty_input() -> None:
